4c06bc60e0b4f7ab66e3a33e39a9246498e2af06
Hashversion <110537156+Hashversion@users.noreply.github.com>
2025-09-25 13:30:38 +0530

    feat: add basic metadata & replace landing page info

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -7,10 +7,9 @@ const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: {
-    default: "Vazen :: Strict monorepo starter",
-    template: "%s :: Vazen",
+    default: "Powenel :: An independent creative experience studio.",
+    template: "%s :: Powenel",
   },
-  description: "A strict monorepo starter I built to kickstart projects, ideas, and experiments.",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
